30de4c19ba7bdb56ff966a20a26dae4d26245757
v-peiz <v-peiz@V-PEIZ_XA02>
2018-11-06 19:01:32 +0800

    Add project files.

---
+++ Multiplication-table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 10; a++)  
+            {
+                for (int b = 1; b < 10; b++)   
+                {
+                    if (b <= a)
+                    {
+                        Console.Write("{0}*{1}={2} \t", a, b, a * b);    
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+
